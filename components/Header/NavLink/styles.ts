55247fd383113a0335eeae8f61d4ae622eac8f6c
gzbrdr <gabrielzbardini@gmail.com>
2022-08-10 11:35:32 -0300

    cor dos textos header

--- components/Header/NavLink/styles.ts
+++ components/Header/NavLink/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Title = styled.p`
-    color: black;
+    color: white;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; 
     
 `
